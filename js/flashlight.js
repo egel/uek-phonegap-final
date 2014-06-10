397a680cf459c236ea2adeeb7aaf4c74565425a7
Maciej Sypien <maciejsypien@gmail.com>
2014-06-11 01:33:05 +0200

    Removed compass; Added Flashlight

---
+++ js/flashlight.js
@@ -0,0 +1,24 @@
+function flashlightToggle()
+{
+    //window.plugins.flashlight.switchOn(success,fail,false);
+    window.plugins.flashlight.toggle(success,fail,false);
+    //window.plugins.toast.show('flashlight toggled','short','center',false);
+    if(lightstate)
+    {
+        window.plugins.toast.show('light off','short','center',false);
+        lightstate = false;
+    }else
+    {
+        window.plugins.toast.show('light on','short','center',false);
+        lightstate = true;
+    }
+}
+function success()
+{
+    console.log("Success");
+}
+function fail()
+{
+    console.log("Fail");
+}
+var lightstate = false;
